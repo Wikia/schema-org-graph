32d43d4f3f58066084ae00dcbe2ffd176c79d348
macbre <maciej.brencz@gmail.com>
2019-01-15 15:08:46 +0100

    Add query_football_graph script and graph visualization HTML

--- grapher/grapher/scripts/query_football_graph.py
+++ grapher/grapher/scripts/query_football_graph.py
@@ -2,6 +2,7 @@
 Ask a question for a football graph
 """
 # https://wikia-inc.atlassian.net/browse/CORE-28
+import json
 import logging
 
 from grapher.graph import RedisGraph
@@ -34,6 +35,7 @@ def nationalities_in_league(nationality, league):
     """
     :type nationality str
     :type league str
+    :rtype: list[dict]
     """
     logging.info('Looking for players from %s in %s', nationality, league)
 
@@ -44,12 +46,77 @@ def nationalities_in_league(nationality, league):
     """
 
     matches = query_redis('football', query, nationality=nationality, league=league)
+    matches = list(matches)
 
     print("\n".join([str(match) for match in matches]))
+
+    return matches
 
 
 def index():
     """
     Script's entry point
     """
-    nationalities_in_league('Iceland', 'Premier League')
+    # nationalities_in_league('Iceland', 'Premier League')
+    matches = nationalities_in_league('Germany', 'Premier League')
+
+    #
+    # generate a graph for visjs library
+    #
+    logging.info('Building a graph data for visjs')
+
+    # mapping of matches field to node group, these fields will be a graph node when visualized
+    # http://visjs.org/docs/network/nodes.html
+    nodes_fields = {
+        't.name': 'SportsTeam',
+        'p.name': 'Person',
+    }
+
+    # http://visjs.org/docs/network/edges.html
+    egde_fields = {
+        'athletee': ('p.name', 't.name')  # athletee relation will connect p.name -> t.name nodes
+    }
+
+    nodes = dict()
+
+    for match in matches:
+        for node_field, node_group in nodes_fields.items():
+            node_id = '{}:{}'.format(match[node_field], node_group)
+
+            if node_id not in nodes:
+                logging.info('Adding a node: %s', node_id)
+
+                nodes[node_id] = {
+                    'id': node_id,
+                    'label': match[node_field],
+                    'group': node_group,
+                }
+
+    nodes = list(nodes.values())
+
+    logging.info('Nodes added: %d', len(nodes))
+
+    # now build edges
+    edges = []
+
+    for match in matches:
+        for edge_type, (from_field, to_field) in egde_fields.items():
+            edge_from = '{}:{}'.format(match[from_field], nodes_fields[from_field])
+            edge_to = '{}:{}'.format(match[to_field], nodes_fields[to_field])
+
+            logging.info('Adding an edge: %s -> %s', edge_from, edge_to)
+
+            edges.append({
+                'from': edge_from,
+                'to': edge_to,
+                'label': edge_type,
+                'arrows': 'to',
+            })
+
+    logging.info('Edges added: %d', len(edges))
+
+    logging.info('Cut here :) ====== ')
+    print('var data = ' + json.dumps({
+        'nodes': nodes,
+        'edges': edges,
+    }))
